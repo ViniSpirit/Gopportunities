973fe700fae4ac2ad495c778ddc8849cd65e72c5
Marcos Vinicius <marcos.silva@wefit.com.br>
2024-10-30 04:02:54 -0300

    feat: add custom logger

--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "github.com/ViniSpirit/Gopportunities/router"
+import (
+	"github.com/ViniSpirit/Gopportunities/config"
+	"github.com/ViniSpirit/Gopportunities/router"
+)
+
+var (
+	logger *config.Logger
+)
 
 func main() {
+	//Initialize configs
+	logger = config.GetLogger("main")
+
+	err := config.Init()
+	if err != nil {
+		logger.ErrorF("Config inicialization erro: %v", err)
+		return
+	}
+
 	router.Inicialize()
 }
