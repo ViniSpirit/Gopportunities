d2bc3b3bcc15b38ea2d62246409c932dac33e65d
Marcos Vinicius <marcos.silva@wefit.com.br>
2024-11-06 20:03:27 -0300

    feat: add list openings handler

--- handler/listOpenings.go
+++ handler/listOpenings.go
@@ -3,11 +3,17 @@ package handler
 import (
 	"net/http"
 
+	"github.com/ViniSpirit/Gopportunities/schemas"
 	"github.com/gin-gonic/gin"
 )
 
 func ListOpeningsHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"msg": "GET opening",
-	})
+	openings := []schemas.Opening{}
+
+	if err := db.Find(&openings).Error; err != nil {
+		sendErrorResponse(ctx, http.StatusInternalServerError, "error listing openings")
+		return
+	}
+
+	sendSuccessResponse(ctx, "list-openings", openings)
 }
